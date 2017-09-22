c7759293174a0576e8347c5e6281512c5753bea4
Anand Doshi <hello@apd.is>
2017-09-22 09:13:15 -0400

    started class Photo

--- flickr_response.py
+++ flickr_response.py
@@ -4,3 +4,10 @@ with open('sample_diction.json') as f:
     diction_string = f.read()
 
 flickr_response = json.loads(diction_string)
+photo_diction = flickr_response['photo']
+
+class Photo:
+    def __init__(self, photo):
+        pass
+
+Photo(photo_diction)
